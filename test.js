e2355373c851d4ba495485f831b3f442ae2556be
Yujia Luo <yarmyarch@live.cn>
2014-01-15 09:34:39 -0800

    demo page updated

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var seq = new Midi.Sequence(1, 24),
     track = seq.getTrack(0);
+
 track.addEvent(0, new Midi.MidiMessage(0xc0, 0x11));
 track.addEvent(0, new Midi.MidiMessage(0xB0, [124,0])); // omni on
 track.addEvent(0, new Midi.MidiMessage(0xB0, [126,1])); // mono on & omni on
@@ -8,7 +9,6 @@ track.addEvent(120, new Midi.MidiMessage(0xB0, [1,127]));
 track.addEvent(240, new Midi.MidiMessage(0x80, [60,127]));
 
 for (var i = 1; i <= 240; ++i) {
-    //~ track.addEvent(i, new Midi.MidiMessage(0xA0, [60, 127 - ~~((127 / 240) * i)]));
     track.addEvent(i, new Midi.MidiMessage(0xB0, [11, 127 - ~~((127 / 240) * i)]));
     track.addEvent(96 + i, new Midi.MidiMessage(0xE0, [0, 64 + ~~((63 / 144) * i)]));
 }
